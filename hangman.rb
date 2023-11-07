36bd58ca05aba2db2ab2e213a6696125beedb54a
Rene Kumsta <rck@sequensis.de>
2023-11-07 18:03:58 +0100

    Update main file

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,8 @@
 dict_file = File.read("hangman.txt")
 
 dictionary = dict_file.split(" ")
-# word_length_dict = dictionary.select { |word| word.length > 4 && word.length < 13 }
-# random_word = word_length_dict.sample
-random_word = 'paintball'
+word_length_dict = dictionary.select { |word| word.length > 4 && word.length < 13 }
+random_word = word_length_dict.sample
 
 # puts random_word
 
