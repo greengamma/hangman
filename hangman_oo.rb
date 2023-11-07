d8c26d3e94885b57326b41f15941a30307e6865b
Rene Kumsta <rck@sequensis.de>
2023-11-07 19:25:53 +0100

    More code refactoring to OOP

--- hangman_oo.rb
+++ hangman_oo.rb
@@ -3,12 +3,12 @@ require 'json'
 class Game
   attr_accessor :player, :score, :level
 
-def initialize()
-  dict_file = File.read("hangman.txt")
-  dictionary = dict_file.split(" ")
-  word_length_dict = dictionary.select { |word| word.length > 4 && word.length < 13 }
-  random_word = word_length_dict.sample
-  word_length = random_word.length
+def initialize
+  @dict_file = File.read("hangman.txt")
+  @dictionary = dict_file.split(" ")
+  @word_length_dict = dictionary.select { |word| word.length > 4 && word.length < 13 }
+  @random_word = word_length_dict.sample
+  @word_length = random_word.length
 end
 
 end
