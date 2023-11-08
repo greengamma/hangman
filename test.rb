28dd3d3d45813a3913fd10c22fc5176a4a04c66e
Rene Kumsta <rck@sequensis.de>
2023-11-08 18:45:35 +0100

    Add def to select random word

--- test.rb
+++ test.rb
@@ -5,27 +5,36 @@ class Game
 
 def initialize(file_path)
   @file_path = file_path
-  @file_contents = ''
+  @dict_file = ''
+  @dictionary = ''
+  @word_length_dict
+  @word_length = 0
+  @random_word = ''
 end
 
 def load_file
   if File.exist?(@file_path)
-    @file_contents = File.read(@file_path)
+    @dict_file = File.read(@file_path)
   else
     puts "File not found!"
   end
 end
 
 def print_file
-  puts @file_contents
+  puts @dict_file
 end
 
 def create_random_word
-
+  @dictionary = @dict_file.split(" ")
+  @word_length_dict = @dictionary.select { |word| word.length > 4 && word.length < 13 }
+  @random_word = @word_length_dict.sample
+  @word_length = @random_word.length
+  puts @random_word
+  puts @word_length
 end
 
 end
 
 game = Game.new("hangman.txt")
 game.load_file
-game.print_file
+game.create_random_word
