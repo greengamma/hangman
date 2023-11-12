1d441cd0f10f2bbe6aaba2cda3523c5881e70194
Rene Kumsta <rck@sequensis.de>
2023-11-12 16:47:29 +0100

    Update code

--- test.rb
+++ test.rb
@@ -39,9 +39,54 @@ def create_hidden_word
   puts @hidden_word
 end
 
+def check_letter
+  @@positions = []
+  if @random_word == @user_letter
+    puts "You won, the word is '#{@random_word}'!"
+    return true
+  elsif @random_word.include?(@user_letter)
+    @random_word.each_char.with_index do |char, index|
+      @positions << index if char == @user_letter
+      # fill hidden word with correct user letters at specific pos
+      @positions.each do |pos|
+        @hidden_word[pos] = @user_letter
+      end
+      if @hidden_word.include?('_')
+        next
+      else
+        puts "You won, the word is '#{@hidden_word}'!"
+        return true
+      end
+    end
+    puts @hidden_word
+  else
+    puts "not included"
+  end
+end
+
 end
 
 game = Game.new("hangman.txt")
 game.load_file
 game.create_random_word
 game.create_hidden_word
+
+
+until game_over
+  puts "Input a letter as your guess..."
+  @user_letter = gets.chomp.downcase
+
+  if game_over = check_letter(random_word, @user_letter, hidden_word)
+    break
+  else
+    attempts += 1
+    remaining_attempts = check_counter(attempts, word_length)
+  end
+  if remaining_attempts == 0
+    puts "You lost! The word was '#{random_word}.'"
+    game_over = true
+  else
+    puts remaining_attempts
+  end
+
+end
